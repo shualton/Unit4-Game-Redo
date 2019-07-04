1093536d9ee42eadb20883f5f350305711f56591
shualton <shualton@gmail.com>
2019-07-04 00:16:54 -0700

    added arrays and variables

---
+++ assets/javascript/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+    var random = [];
+    for(var r = 20; r <= 120; r++) {
+        random.push(r);
+    }
+    var powerup = [];
+    for(var p = 1; p < 10; p++) {
+        powerup.push(p);
+    }
+
+    var randNum;
+    var powerNum = [];
+    var p1;
+    var p2;
+    var p3;
+    var p4;
+
+
+});
